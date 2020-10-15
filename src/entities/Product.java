8682e73c7400e18d5adea6e08347dd69d547f0b4
Ricardo Martins <rs84martins@gmail.com>
2020-10-14 22:01:46 -0300

    Projeto Criado

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -23,8 +23,7 @@ public class Product {
 	public void setPrice(Double price) {
 		this.price = price;
 	}
-	
-	
+		
 	@Override
 	public String toString() {
 		return "Product [name=" + name + ", price=" + price + "]";
